66fb1b2e912f78724a05650cf718f8d68c2cbb6b
cyanamous <rickyaditya.2711@gmail.com>
2018-10-09 11:43:08 +0530

    First Commit

--- run_dataset.py
+++ run_dataset.py
@@ -11,7 +11,8 @@ sess = tf.InteractiveSession()
 saver = tf.train.Saver()
 saver.restore(sess, "save/model.ckpt")
 
-img = cv2.imread('steering_wheel.jpg',0)
+# 0 indiactes color image
+img = cv2.imread('steering_wheel_image.jpg',0)
 rows,cols = img.shape
 
 smoothed_angle = 0
@@ -32,19 +33,21 @@ with open("driving_dataset/data.txt") as f:
 num_images = len(xs)
 
 
-i = math.ceil(num_images*0.8)
+i = math.ceil(num_images*0.8) #using after 80% of images i.e. Test Images
 print("Starting frameofvideo:" +str(i))
 
+#Press q to stop 
 while(cv2.waitKey(10) != ord('q')):
+    # Driving Test Image displayed as Video
     full_image = scipy.misc.imread("driving_dataset/" + str(i) + ".jpg", mode="RGB")
     image = scipy.misc.imresize(full_image[-150:], [66, 200]) / 255.0
     degrees = model.y.eval(feed_dict={model.x: [image], model.keep_prob: 1.0})[0][0] * 180.0 / scipy.pi
     #call("clear")
-    #print("Predicted Steering angle: " + str(degrees))
     print("Steering angle: " + str(degrees) + " (pred)\t" + str(ys[i]*180/scipy.pi) + " (actual)")
     cv2.imshow("frame", cv2.cvtColor(full_image, cv2.COLOR_RGB2BGR))
     #make smooth angle transitions by turning the steering wheel based on the difference of the current angle
     #and the predicted angle
+    #Angle at which the steering wheel image should be rotated
     smoothed_angle += 0.2 * pow(abs((degrees - smoothed_angle)), 2.0 / 3.0) * (degrees - smoothed_angle) / abs(degrees - smoothed_angle)
     M = cv2.getRotationMatrix2D((cols/2,rows/2),-smoothed_angle,1)
     dst = cv2.warpAffine(img,M,(cols,rows))
